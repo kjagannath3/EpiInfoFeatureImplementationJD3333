ca324a6246392f9f20448c47e0f2dd60cc447b23
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2023-11-24 20:28:32 -0500

    box layout tinkering

--- app/src/main/java/com/example/epiinfofeatureimplementation/MainActivity.kt
+++ app/src/main/java/com/example/epiinfofeatureimplementation/MainActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.epiinfofeatureimplementation.ui.theme.EpiInfoFeatureImplementationTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,16 +41,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello, my name is Keshav, this is  $name!",
-
-        color = Color.Black,
+    Box (
+        contentAlignment = Alignment.Center,
         modifier = modifier
-            .background(Color.Cyan)
-            .padding(16.dp)
-            .background(Color.Green)
+            .size(400.dp)
+    ){
+        Text(
+            text = "Hello, my name is Keshav, this is  $name!",
 
-    )
+            color = Color.Black,
+            modifier = modifier
+                .align(Alignment.BottomEnd)
+        )
+        Text(
+            text = "Some other text",
+            color = Color.Blue,
+            fontSize = 30.sp,
+            modifier = modifier
+                .align(Alignment.TopCenter)
+
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
