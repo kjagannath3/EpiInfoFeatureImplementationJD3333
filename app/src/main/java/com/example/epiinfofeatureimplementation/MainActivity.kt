41f600f14810cf341edb41294a50119f7acfbc6d
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2023-11-24 20:14:07 -0500

    More tinkering

--- app/src/main/java/com/example/epiinfofeatureimplementation/MainActivity.kt
+++ app/src/main/java/com/example/epiinfofeatureimplementation/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.epiinfofeatureimplementation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.epiinfofeatureimplementation.ui.theme.EpiInfoFeatureImplementationTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,8 +37,13 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello, my name is Keshav, this is  $name!",
-        modifier = modifier,
-        color = Color.Black
+
+        color = Color.Black,
+        modifier = modifier
+            .background(Color.Cyan)
+            .padding(16.dp)
+            .background(Color.Green)
+
     )
 }
 
