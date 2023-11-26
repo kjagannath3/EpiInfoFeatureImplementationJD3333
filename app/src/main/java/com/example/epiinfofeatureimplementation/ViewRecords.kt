5da81c91c95f52eca8676fca4d3b0b316916a7d4
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2023-11-26 15:20:17 -0500

    refactored home button

--- app/src/main/java/com/example/epiinfofeatureimplementation/ViewRecords.kt
+++ app/src/main/java/com/example/epiinfofeatureimplementation/ViewRecords.kt
@@ -8,12 +8,15 @@ import android.content.Intent
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material3.ListItem
@@ -31,7 +34,9 @@ import com.example.epiinfofeatureimplementation.ui.theme.EpiInfoFeatureImplement
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 
 
 class ViewRecords : ComponentActivity() {
@@ -40,6 +45,7 @@ class ViewRecords : ComponentActivity() {
         setContent {
             EpiInfoFeatureImplementationTheme {
                 RecyclerView()
+                HomeButton()
             }
         }
 
@@ -49,6 +55,31 @@ class ViewRecords : ComponentActivity() {
 
     }
 }
+
+@Composable
+fun HomeButton() {
+    val context = LocalContext.current
+    Row(
+        verticalAlignment = Alignment.Top,
+        modifier = Modifier
+            .padding(top = 13.dp, start = 16.dp, end = 16.dp)
+            .fillMaxWidth()
+    ) {
+        Text("Title", Modifier.weight(1f)) // Replace with your actual title composable
+
+        // Your home button
+        Image(
+            painter = painterResource(id = R.drawable.home_button),
+            contentDescription = "Home",
+            modifier = Modifier
+                .size(width = 29.dp, height = 30.dp)
+                .clickable { val intent = Intent(context, MainActivity::class.java)
+                    context.startActivity(intent) }
+        )
+    }
+}
+
+
 
 @Composable
 fun RecordListItem(name : String) {
