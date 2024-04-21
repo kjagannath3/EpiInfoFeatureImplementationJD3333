939d809bb1eaa3a4f8a3c501fe408a5e8ec02540
Dixit, Rhea <rheadixit@yahoo.com>
2024-04-21 12:34:56 -0400

    fixing FrequencyView

--- epi-info-android-companion/AppCode/src/main/java/gov/cdc/epiinfo/analysis/FrequencyView.java
+++ epi-info-android-companion/AppCode/src/main/java/gov/cdc/epiinfo/analysis/FrequencyView.java
@@ -63,9 +63,9 @@ public class FrequencyView extends RelativeLayout {
 
 		String[] stringValues = new String[formMetadata.DataFields.size() + 1];
 		stringValues[0] = context.getString(R.string.analysis_select);
-		for (int x=1;x<=formMetadata.DataFields.size();x++)
+		for (int x = 1;x <= formMetadata.DataFields.size(); x++)
 		{
-			stringValues[x] = formMetadata.DataFields.get(x-1).getName();
+			stringValues[x] = formMetadata.DataFields.get(x - 1).getName();
 		}
 
 		outputLayout = this.findViewById(R.id.freqOutput);
@@ -148,6 +148,7 @@ public class FrequencyView extends RelativeLayout {
 
 							outputLayout.addView(header);
 
+							//this gets the total count of the items in this file
 							int totalCount = 0;
 							do{
 								totalCount += c.getInt(c.getColumnIndexOrThrow("COUNT(*)"));
@@ -236,16 +237,16 @@ public class FrequencyView extends RelativeLayout {
 								txtValue.setGravity(Gravity.LEFT);
 								row.addView(txtValue);
 
-								//int totalCount = c.getCount();
+								//totalCount was established earlier
 
 								TextView txtCount = new TextView(context);
 								txtCount.setText(count + " ");
-								//totalCount += count;
 								txtCount.setLayoutParams(cellParams);
 								txtCount.setGravity(Gravity.CENTER);
 								txtCount.setTypeface(null, Typeface.BOLD);
 								row.addView(txtCount);
 
+								//this code here is what is displaying the frequency
 								TextView txtPer = new TextView(context);
 								txtPer.setText(String.format("%.2f", ((double) count/totalCount) * 100) + "%");
 								txtPer.setLayoutParams(cellParams);
