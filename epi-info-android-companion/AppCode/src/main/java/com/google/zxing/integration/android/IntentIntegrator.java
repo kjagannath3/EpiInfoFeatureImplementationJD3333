41293d950102cfcc30c160bf5272ccf31a6d8d21
Jade Spooner <90151016+ControlledChaos0@users.noreply.github.com>
2024-04-21 21:09:06 -0400

    Jade barcode (#74)
    * Fixed Map Viewer
    
    * Quick Depreciation Fix

--- epi-info-android-companion/AppCode/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
+++ epi-info-android-companion/AppCode/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
@@ -18,7 +18,7 @@ package com.google.zxing.integration.android;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
